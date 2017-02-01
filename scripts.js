c9cb001175c2c83cd3ef414bdfd6ece6684813c9
aNewDev <iam@anewdev.io>
2017-02-01 13:36:46 -0600

    Closes #3 Added way to change from F to C
    Added way to change from F to C

--- scripts.js
+++ scripts.js
@@ -24,25 +24,18 @@ function geoFindMe() {
   function success(position) {
     latitude  = position.coords.latitude;
     longitude = position.coords.longitude;
-
-    output.innerHTML = 'Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°';
-
     getWeatherDetail();
-
     }
 
   function error() {
     output.innerHTML = "Unable to retrieve your location";
   }
 
-  output.innerHTML = "Locating…";
-
   navigator.geolocation.getCurrentPosition(success, error);
 }
 
 // Pull the weather from openweathermap and post the weather on the page
 function getWeatherDetail() {
-  //weatherURL = ;
 
   var output = $.ajax({
     url: "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=imperial&APPID=09449d6c9c3b937d6a67e55e34b1bfab",
@@ -52,11 +45,10 @@ function getWeatherDetail() {
     success: function(data) {
     // assign the weatherData for future use, and set the page values to current weather
       weatherData = data;
-      console.log(weatherData);
-      document.getElementById("city").innerHTML = weatherData.name;
-     	document.getElementById("temp").innerHTML = Math.round(weatherData.main.temp) + '° F';
-      document.getElementById("conditions").innerHTML = weatherData.weather[0].description;
       changeBackgroundImg(weatherData.weather[0].main);
+      getFahrenheit();
+      document.getElementById("city").innerHTML = weatherData.name;
+      document.getElementById("conditions").innerHTML = weatherData.weather[0].description;
     },
     error: function(err) { alert(err); },
   });
@@ -66,4 +58,15 @@ function getWeatherDetail() {
 
 function changeBackgroundImg(group) {
   document.body.style.backgroundImage = "url(https://s3.amazonaws.com/assetsanewdevio/fccweather/" + group + ".jpg)";
+}
+
+// get celsius temp from fahrenheit
+
+function getCelsius() {
+  document.getElementById("temp").innerHTML = Math.round((weatherData.main.temp - 32) * 5 / 9) + '° <a onclick="getFahrenheit()" href="#">C</span>';
+}
+
+// get fahrenheit from api data
+function getFahrenheit() {
+  document.getElementById("temp").innerHTML = Math.round(weatherData.main.temp) + '° <a onclick="getCelsius()" href="#">F</span>';
 }
